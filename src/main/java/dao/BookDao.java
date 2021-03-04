9df659ba67a843b3eeea189f0b843b6fcf52ffc4
Nikolay Rybakov <ni_fanta@mail.ru>
2021-03-05 00:22:49 +0300

    changed interface ctrl+alt+L

--- src/main/java/dao/BookDao.java
+++ src/main/java/dao/BookDao.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface BookDao {
     void addBook(Book book);
+
     void updateBook(Book book);
+
     void removeBook(int id);
+
     Book getBookById(int id);
+
     List<Book> listBooks();
 }
